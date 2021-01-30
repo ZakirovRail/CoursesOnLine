a1c442803f07ab64f3e70cfb59783c65b95be880
zakirovrj <rjzakirov@gmail.com>
2021-01-30 20:15:49 +0100

    HackerRank - strings exercises

---
+++ HackerRank/strings/Alphabet_Rangoli.py
@@ -0,0 +1,17 @@
+import string
+# alpha = string.ascii_lowercase
+# print(alpha)  # abcdefghijklmnopqrstuvwxyz
+
+
+def print_rangoli(size):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    L = []
+    for i in range(size):
+        s = "-".join(alpha[i:size])
+        L.append((s[::-1]+s[1:]).center(4*size-3, "-"))
+    print('\n'.join(L[:0:-1]+L))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
