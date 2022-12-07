200c186a90d3b4c6c23877592b34d48be1294b2a
zakirovrj <rjzakirov@gmail.com>
2022-12-07 07:00:24 +0100

    Chapter 3 for Pytest - partial

---
+++ Books/python_testing_with_pytest/ch3/test_rename_fixture.py
@@ -0,0 +1,19 @@
+"""Demonstrate fixture renaming."""
+
+import pytest
+
+
+@pytest.fixture(name='lue')
+def ultimate_answer_to_life_the_universe_and_everything():
+    """Return ultimate answer."""
+    return 42
+
+
+def test_everything(lue):
+    """Use shorter name."""
+    assert lue == 42
+
+
+
+
+
